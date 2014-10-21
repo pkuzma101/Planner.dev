f53080cccefab50cf415fc39d93b140b00bf11ee
pkuzma101 <kuzma.paul@gmail.com>
2014-10-21 11:10:07 -0500

    Exericse 5.3.5 - fixed errors

--- js/breakContinueJS.js
+++ js/breakContinueJS.js
@@ -1,9 +1,18 @@
-var random = Math.floor((Math.random()*50)+1);
+
+do {
+	var random = Math.floor((Math.random()*50)+1);
+} while(random % 2 == 0);
 
 for(var i = 1; i < 50; i++) {
-	if(i % 2 == 0) {
+	if(random == i) {
+		console.log("Yipes!  We are skipping " + i);
 		continue;
 	}
+	else if(i % 2 != 0) {
+		console.log("Here is an odd number: " + i);
+	}
 
-	console.log("Here is an odd number: " + i);
+
+	
 }
+
