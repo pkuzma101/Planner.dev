3685fa0273078ca4aaa006d48903a4135d59b490
pkuzma101 <kuzma.paul@gmail.com>
2014-10-21 15:42:08 -0500

    Exercise 5.5

---
+++ js/iteratingArraysJS.js
@@ -0,0 +1,19 @@
+// todo:
+// Create an array of 4 names using literal array notation, in a variable called 'names'.
+
+var names = ["Terra", "Locke", "Edgar", "Celes"];
+
+// todo:
+// Create a log statement that will log the number of elements in the names array.
+console.log("There are " + names.length + " names in the array.");
+
+// todo:
+// Create log statements that will print each of the names array elements individually.
+
+console.log("The first character is " + names[0] + ".");
+
+console.log("The second character is " + names[1] + ".");
+
+console.log("The third character is " + names[2] + ".");
+
+console.log("The fourth character is " + names[3] + ".");
