5196fdd9bfecb84eee07e42ed0c2306db079dd03
pkuzma101 <kuzma.paul@gmail.com>
2014-10-22 09:41:43 -0500

    Exercise 5.5.1

--- js/iteratingArraysJS.js
+++ js/iteratingArraysJS.js
@@ -10,10 +10,18 @@ console.log("There are " + names.length + " names in the array.");
 // todo:
 // Create log statements that will print each of the names array elements individually.
 
-console.log("The first character is " + names[0] + ".");
+// console.log("The first character is " + names[0] + ".");
 
-console.log("The second character is " + names[1] + ".");
+// console.log("The second character is " + names[1] + ".");
 
-console.log("The third character is " + names[2] + ".");
+// console.log("The third character is " + names[2] + ".");
 
-console.log("The fourth character is " + names[3] + ".");
+// console.log("The fourth character is " + names[3] + ".");
+
+for(var i = 0; i < names.length; i++) {
+	console.log("The name at index " + i + " is: " + names[i]);
+}
+
+names.forEach(function(element, index, array) {
+	console.log("The name at index " + index + " is: " + element);
+});
