bc1b42359e3ef0f3d24f625e460ccedd800a6ce5
pkuzma101 <kuzma.paul@gmail.com>
2014-11-04 09:28:34 -0600

    Exercise 9.1.1

---
+++ js/jqueryExercises.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	var first = $('#first').html();
+	alert(first);
+
+	var list = $('.list').html();
+	alert(list);
+
+	var reasons = $('#reasons').html();
+	alert(reasons);
+});
